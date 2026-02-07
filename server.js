3a617383d1a8aecb92aa0f8259c8eb07891d6160
Naitik <naitikraiyani88@gmail.com>
2026-02-07 17:05:51 +0530

    Include images in Vercel build and restore CORS headers

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(cors({
 }));
 // app.use(cors());
 app.use(express.json());
-app.use('/images', express.static('images'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 mongoose.connect(process.env.MONGODBCON)
   .then(() => console.log('Connected to MongoDB'))
