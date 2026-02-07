eed5ed3a08cfdea4643a9c700360e183a32a88c6
Naitik <naitikraiyani88@gmail.com>
2026-02-07 17:34:43 +0530

    Restore Vercel deployment fixes and clean up logic

--- server.js
+++ server.js
@@ -9,10 +9,13 @@ import User from './model/userModel.js';
 import GMGame from './model/gmGameModel.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 8000;
 
@@ -35,7 +38,7 @@ app.use(cors({
 }));
 // app.use(cors());
 app.use(express.json());
-app.use('/images', express.static('images'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 mongoose.connect(process.env.MONGODBCON)
   .then(() => console.log('Connected to MongoDB'))
@@ -417,3 +420,5 @@ app.post('/login', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+export default app;
