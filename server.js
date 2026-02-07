c401248a42d0540d2706b5b9a868c572ec46cc6a
Naitik <naitikraiyani88@gmail.com>
2026-02-07 17:28:18 +0530

    add

--- server.js
+++ server.js
@@ -9,13 +9,10 @@ import User from './model/userModel.js';
 import GMGame from './model/gmGameModel.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 8000;
 
@@ -38,11 +35,9 @@ app.use(cors({
 }));
 // app.use(cors());
 app.use(express.json());
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/images', express.static('images'));
 
-mongoose.connect(process.env.MONGODBCON, {
-  serverSelectionTimeoutMS: 5000 // Timeout after 5s instead of hanging
-})
+mongoose.connect(process.env.MONGODBCON)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
@@ -422,5 +417,3 @@ app.post('/login', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-export default app;
